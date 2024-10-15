0753fb5f7ce6bd0f27da9cb71ee4ba031a5a4112
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-15 14:27:10 -0400

    Chapter 5: add player that can move around

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod map;
+mod player;
 
 mod prelude {
     pub use crate::map::*;
+    pub use crate::player::*;
     pub use bracket_lib::prelude::*;
 
     pub const SCREEN_HEIGHT: i32 = 50;
@@ -21,17 +23,23 @@ fn main() -> BError {
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        State { map: Map::new() }
+        State {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
