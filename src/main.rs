29a0c4862993dc496982d1749f2648b663dee01f
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-15 14:57:30 -0400

    Chapter 5: build dungeon

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod map;
+mod map_builder;
 mod player;
 
 mod prelude {
     pub use crate::map::*;
+    pub use crate::map_builder::*;
     pub use crate::player::*;
     pub use bracket_lib::prelude::*;
 
@@ -28,9 +30,12 @@ struct State {
 
 impl State {
     fn new() -> Self {
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
         State {
-            map: Map::new(),
-            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
         }
     }
 }
