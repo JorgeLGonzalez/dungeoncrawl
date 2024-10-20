1914af2487b8e1eb9a177cc0bb4ba4a0d71099e6
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-20 18:01:14 -0400

    refactor(combat)

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -4,25 +4,33 @@ use crate::prelude::*;
 #[read_component(WantsToAttack)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
-    let mut attackers = <(Entity, &WantsToAttack)>::query();
-    let victims: Vec<(Entity, Entity)> = attackers
-        .iter(ecs)
-        .map(|(entity, attack)| (*entity, attack.victim))
-        .collect();
-    victims.iter().for_each(|(message, victim)| {
-        if let Ok(mut health) = ecs
-            .entry_mut(*victim)
-            .unwrap()
-            .get_component_mut::<Health>()
-        {
-            println!("Health before attack: {}", health.current);
-            health.current -= 1;
-            if health.current < 1 {
+    gather_victims(ecs).iter().for_each(|(message, victim)| {
+        damage(ecs, *victim)
+            .filter(|health| *health < 1)
+            .inspect(|_| {
+                println!("\tVictim Died :-(");
                 commands.remove(*victim);
-            }
-            println!("Health after attack: {}", health.current);
-        }
+            });
 
         commands.remove(*message);
     });
+}
+
+fn gather_victims(ecs: &SubWorld) -> Vec<(Entity, Entity)> {
+    <(Entity, &WantsToAttack)>::query()
+        .iter(ecs)
+        .map(|(entity, attack)| (*entity, attack.victim))
+        .collect()
+}
+
+fn damage(ecs: &mut SubWorld, victim: Entity) -> Option<i32> {
+    if let Ok(health) = ecs.entry_mut(victim).unwrap().get_component_mut::<Health>() {
+        println!("Health before attack: {}", health.current);
+        health.current -= 1;
+        println!("Health after attack: {}", health.current);
+
+        Some(health.current)
+    } else {
+        None
+    }
 }
