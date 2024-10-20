00d33ae4c91b548b984baf626aa2137e451a1d2b
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-20 19:38:42 -0400

    refactor(random_move)

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -12,32 +12,60 @@ pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     <(Entity, &Point, &MovingRandomly)>::query()
         .iter(ecs)
         .for_each(|(entity, pos, _)| {
-            let destination = match rng.range(0, 4) {
-                0 => Point::new(-1, 0),
-                1 => Point::new(1, 0),
-                2 => Point::new(0, -1),
-                _ => Point::new(0, 1),
-            } + *pos;
+            let destination = determine_destination(&mut rng, *pos);
 
-            let mut attacked = false;
-            positions
+            let occupants: Vec<Occupant> = positions
                 .iter(ecs)
-                .filter(|(_, target_pos, _)| **target_pos == destination)
-                .for_each(|(victim, _, _)| {
-                    if ecs
-                        .entry_ref(*victim)
-                        .unwrap()
-                        .get_component::<Player>()
-                        .is_ok()
-                    {
-                        commands.push(((), WantsToAttack::new(*entity, *victim)));
-                    }
+                .filter(|p| destination_occupied(destination, p))
+                .map(|(victim, _, _)| identify_occupant(ecs, *victim))
+                .collect();
 
-                    attacked = true;
+            // TODO fn that returns attacks on player that we then push to commands
+            occupants
+                .iter()
+                .filter_map(|o| match *o {
+                    Occupant::Player(p) => Some(WantsToAttack::new(*entity, p)),
+                    _ => None,
+                })
+                .for_each(|attack| {
+                    commands.push(((), attack));
                 });
 
-            if !attacked {
+            if occupants.is_empty() {
                 commands.push(((), WantsToMove::new(destination, *entity)));
             }
         });
+}
+
+fn destination_occupied(destination: Point, (_, pos, _): &(&Entity, &Point, &Health)) -> bool {
+    **pos == destination
+}
+
+fn determine_destination(rng: &mut RandomNumberGenerator, pos: Point) -> Point {
+    let delta = match rng.range(0, 4) {
+        0 => Point::new(-1, 0),
+        1 => Point::new(1, 0),
+        2 => Point::new(0, -1),
+        _ => Point::new(0, 1),
+    };
+
+    delta + pos
+}
+
+fn identify_occupant(ecs: &SubWorld, occupant: Entity) -> Occupant {
+    if ecs
+        .entry_ref(occupant)
+        .unwrap()
+        .get_component::<Player>()
+        .is_ok()
+    {
+        Occupant::Player(occupant)
+    } else {
+        Occupant::FellowMonster
+    }
+}
+
+enum Occupant {
+    Player(Entity),
+    FellowMonster,
 }
