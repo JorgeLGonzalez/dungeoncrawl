bc01913288faf849df8cba16d64b31f9d861ea92
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-20 18:42:09 -0400

    refactor(player_input)

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -9,36 +9,64 @@ pub fn player_input(
     #[resource] key: &Option<VirtualKeyCode>,
     #[resource] turn_state: &mut TurnState,
 ) {
-    if let Some(key) = *key {
-        let delta = match key {
-            VirtualKeyCode::Left => Point::new(-1, 0),
-            VirtualKeyCode::Right => Point::new(1, 0),
-            VirtualKeyCode::Up => Point::new(0, -1),
-            VirtualKeyCode::Down => Point::new(0, 1),
-            _ => Point::new(0, 0),
-        };
-        let (player, destination) = <(Entity, &Point)>::query()
-            .filter(component::<Player>())
-            .iter(ecs)
-            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
-            .unwrap();
+    key.and_then(delta).map(|delta| {
+        let player = get_player_info(ecs, delta);
+        let attacks = gather_attacks(ecs, &player);
 
-        let will_move = delta.x != 0 || delta.y != 0;
-        if will_move {
-            let attacks: Vec<((), WantsToAttack)> = <(Entity, &Point)>::query()
-                .filter(component::<Enemy>())
-                .iter(ecs)
-                .filter(|(_, pos)| **pos == destination)
-                .map(|(entity, _)| ((), WantsToAttack::new(player, *entity)))
-                .collect();
-
-            if attacks.is_empty() {
-                commands.push(((), WantsToMove::new(destination, player)));
-            } else {
-                commands.extend(attacks);
-            }
+        if attacks.is_empty() {
+            commands.push(((), WantsToMove::new(player.destination, player.player)));
+        } else {
+            commands.extend(attacks);
         }
+    });
 
+    if key.is_some() {
         *turn_state = TurnState::PlayerTurn;
+    }
+}
+
+fn delta(key: VirtualKeyCode) -> Option<Point> {
+    match key {
+        VirtualKeyCode::Left => Some(Point::new(-1, 0)),
+        VirtualKeyCode::Right => Some(Point::new(1, 0)),
+        VirtualKeyCode::Up => Some(Point::new(0, -1)),
+        VirtualKeyCode::Down => Some(Point::new(0, 1)),
+        _ => None,
+    }
+}
+
+fn gather_attacks(ecs: &SubWorld, player_info: &PlayerInfo) -> Vec<((), WantsToAttack)> {
+    let PlayerInfo {
+        destination,
+        player,
+    } = *player_info;
+
+    <(Entity, &Point)>::query()
+        .filter(component::<Enemy>())
+        .iter(ecs)
+        .filter(|(_, pos)| **pos == destination)
+        .map(|(entity, _)| ((), WantsToAttack::new(player, *entity)))
+        .collect()
+}
+
+fn get_player_info(ecs: &SubWorld, delta: Point) -> PlayerInfo {
+    <(Entity, &Point)>::query()
+        .filter(component::<Player>())
+        .iter(ecs)
+        .find_map(|(entity, pos)| Some(PlayerInfo::new(*entity, *pos + delta)))
+        .unwrap()
+}
+
+struct PlayerInfo {
+    destination: Point,
+    player: Entity,
+}
+
+impl PlayerInfo {
+    fn new(player: Entity, destination: Point) -> Self {
+        Self {
+            destination,
+            player,
+        }
     }
 }
