c659aec33a7d09de8f380972b4173f93226e17d0
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-20 15:41:35 -0400

    refactor(tooltip)

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -5,26 +5,31 @@ use crate::prelude::*;
 #[read_component(Name)]
 #[read_component(Point)]
 pub fn tooltip(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
-    // TODO why are positions mut
-    let mut positions = <(Entity, &Point, &Name)>::query();
-    let offset = Point::new(camera.left_x, camera.top_y);
-    let map_pos = *mouse_pos + offset;
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(3);
 
-    positions
+    let map_pos = determine_map_pos(*mouse_pos, camera);
+    <(Entity, &Point, &Name)>::query()
         .iter(ecs)
         .filter(|(_, pos, _)| **pos == map_pos)
         .for_each(|(entity, _, name)| {
-            let screen_pos = *mouse_pos * 4; // adjust to x-layer dims p. 278
-            let display =
-                if let Ok(health) = ecs.entry_ref(*entity).unwrap().get_component::<Health>() {
-                    format!("{}: {} hp", &name.0, health.current)
-                } else {
-                    name.0.clone()
-                };
-            draw_batch.print(screen_pos, &display);
+            let screen_pos = *mouse_pos * 4;
+            draw_batch.print(screen_pos, display(ecs, *entity, &name.0));
         });
 
     draw_batch.submit(10100).expect("Batch error");
+}
+
+fn determine_map_pos(mouse_pos: Point, camera: &Camera) -> Point {
+    let offset = Point::new(camera.left_x, camera.top_y);
+
+    mouse_pos + offset
+}
+
+fn display(ecs: &SubWorld, entity: Entity, name: &str) -> String {
+    if let Ok(health) = ecs.entry_ref(entity).unwrap().get_component::<Health>() {
+        format!("{}: {} hp", name, health.current)
+    } else {
+        name.to_string()
+    }
 }
