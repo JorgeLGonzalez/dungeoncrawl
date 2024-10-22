00a6d97fae3dd44d6aad5bbff2914381ae707363
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-22 11:24:03 -0400

    refactor(chasing)

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -6,45 +6,69 @@ use crate::prelude::*;
 #[read_component(Player)]
 #[read_component(Point)]
 pub fn chasing(#[resource] map: &Map, ecs: &SubWorld, commands: &mut CommandBuffer) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
-    let mut positions = <(Entity, &Point, &Health)>::query();
     let mut player = <(&Point, &Player)>::query();
-
     let player_pos = player.iter(ecs).nth(0).unwrap().0;
     let player_idx = map_idx(player_pos.x, player_pos.y);
 
     let search_targets = vec![player_idx];
     let dijkstra_map = DijkstraMap::new(SCREEN_WIDTH, SCREEN_HEIGHT, &search_targets, map, 1024.0);
 
-    movers.iter(ecs).for_each(|(entity, pos, _)| {
-        let idx = map_idx(pos.x, pos.y);
-        if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
-            let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
-            // see p 315 for rationale for 1.2
-            let destination = if distance > 1.2 {
-                map.index_to_point2d(destination)
-            } else {
-                *player_pos
-            };
-
-            let occupants: Vec<Occupant> = positions
-                .iter(ecs)
-                .filter(|o| occupied(destination, o))
-                .map(|(victim, ..)| identify(ecs, *victim))
-                .collect();
-
-            if let Some(player_to_attack) = find_player(&occupants) {
-                commands.push(((), WantsToAttack::new(*entity, player_to_attack)));
-            } else if occupants.is_empty() {
-                commands.push(((), WantsToMove::new(*entity, destination)));
-            } else {
-                println!(
-                    "Monster unable to move to {:?} already occupied by a fellow monster",
-                    destination
-                );
+    <(Entity, &Point, &ChasingPlayer)>::query()
+        .iter(ecs)
+        .map(|(entity, pos, _)| {
+            determine_action(*entity, *pos, *player_pos, &dijkstra_map, map, ecs)
+        })
+        .for_each(|a| match a {
+            Action::Attack(a) => {
+                commands.push(((), a));
             }
+            Action::Move(m) => {
+                commands.push(((), m));
+            }
+            Action::None => (),
+        });
+}
+
+fn determine_action(
+    monster: Entity,
+    monster_pos: Point,
+    player_pos: Point,
+    dijkstra_map: &DijkstraMap,
+    map: &Map,
+    ecs: &SubWorld,
+) -> Action {
+    let mut positions = <(Entity, &Point, &Health)>::query();
+
+    let idx = map_idx(monster_pos.x, monster_pos.y);
+    if let Some(destination) = DijkstraMap::find_lowest_exit(dijkstra_map, idx, map) {
+        let distance = DistanceAlg::Pythagoras.distance2d(monster_pos, player_pos);
+        // see p 315 for rationale for 1.2
+        let destination = if distance > 1.2 {
+            map.index_to_point2d(destination)
+        } else {
+            player_pos
+        };
+
+        let occupants: Vec<Occupant> = positions
+            .iter(ecs)
+            .filter(|o| occupied(destination, o))
+            .map(|(victim, ..)| identify(ecs, *victim))
+            .collect();
+
+        if let Some(player_to_attack) = find_player(&occupants) {
+            Action::Attack(WantsToAttack::new(monster, player_to_attack))
+        } else if occupants.is_empty() {
+            Action::Move(WantsToMove::new(monster, destination))
+        } else {
+            println!(
+                "Monster unable to move to {:?} already occupied by a fellow monster",
+                destination
+            );
+            Action::None
         }
-    });
+    } else {
+        Action::None
+    }
 }
 
 fn identify(ecs: &SubWorld, occupant: Entity) -> Occupant {
@@ -77,4 +101,11 @@ fn occupied(destination: Point, (_, pos, _): &(&Entity, &Point, &Health)) -> boo
 enum Occupant {
     Player(Entity),
     FellowMonster,
+}
+
+#[derive(PartialEq)]
+enum Action {
+    Attack(WantsToAttack),
+    Move(WantsToMove),
+    None,
 }
