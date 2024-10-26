8b8e213af8386cee40876fede1a1b399d2f6e0e9
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-26 10:19:16 -0400

    ch10: only render visible monsters and their tooltips

--- src/systems/tooltips.rs
+++ src/systems/tooltips.rs
@@ -1,17 +1,22 @@
 use crate::prelude::*;
 
 #[system]
+#[read_component(FieldOfView)]
 #[read_component(Health)]
 #[read_component(Name)]
+#[read_component(Player)]
 #[read_component(Point)]
 pub fn tooltip(ecs: &SubWorld, #[resource] mouse_pos: &Point, #[resource] camera: &Camera) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(ConsoleLayer::Hud.into());
 
     let map_pos = determine_map_pos(*mouse_pos, camera);
     <(Entity, &Point, &Name)>::query()
         .iter(ecs)
-        .filter(|(_, pos, _)| **pos == map_pos)
+        .filter(|(_, pos, _)| **pos == map_pos && player_fov.visible_tiles.contains(&pos))
         .for_each(|(entity, _, name)| {
             let screen_pos = *mouse_pos * 4;
             draw_batch.print(screen_pos, display(ecs, *entity, &name.0));
