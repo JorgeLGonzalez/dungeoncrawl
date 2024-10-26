2541e76d065df937c2dfdd93eb1c6e12e75fb9d8
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-26 16:29:04 -0400

    refactor(movement)

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -18,21 +18,35 @@ pub fn movement(
 
     commands.add_component(want_move.entity, want_move.destination);
 
-    if let Ok(entry) = ecs.entry_ref(want_move.entity) {
-        if let Ok(fov) = entry.get_component::<FieldOfView>() {
-            commands.add_component(want_move.entity, fov.clone_dirty());
+    if let Some((fov, is_player)) = fov(want_move.entity, ecs) {
+        commands.add_component(want_move.entity, fov.clone_dirty());
 
-            if is_player(&entry) {
-                camera.on_player_move(want_move.destination);
-
-                fov.visible_tiles.iter().for_each(|pos| {
-                    map.revealed_tiles[map_idx(pos.x, pos.y)] = true;
-                });
-            }
+        if is_player {
+            handle_player_move(want_move.destination, &fov, map, camera);
         }
     }
 
     commands.remove(*entity);
+}
+
+fn fov(entity: Entity, ecs: &SubWorld) -> Option<(FieldOfView, bool)> {
+    if let Ok(entry) = ecs.entry_ref(entity) {
+        if let Ok(fov) = entry.get_component::<FieldOfView>() {
+            let is_player = is_player(&entry);
+
+            return Some((fov.clone(), is_player));
+        }
+    }
+
+    None
+}
+
+fn handle_player_move(destination: Point, fov: &FieldOfView, map: &mut Map, camera: &mut Camera) {
+    fov.visible_tiles.iter().for_each(|pos| {
+        map.revealed_tiles[map_idx(pos.x, pos.y)] = true;
+    });
+
+    camera.on_player_move(destination);
 }
 
 fn is_player(entry: &EntryRef<'_>) -> bool {
