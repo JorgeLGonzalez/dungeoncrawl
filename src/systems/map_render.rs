bbeb7296e22977c3d9f9e9b46f2dd4a0f0754873
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-10-26 16:07:57 -0400

    refactor(map_render)

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -12,18 +12,10 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
     for y in camera.top_y..=camera.bottom_y {
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
-            let idx = map_idx(x, y);
-            if map.in_bounds(pt)
-                && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx])
-            {
-                let tint = if player_fov.visible_tiles.contains(&pt) {
-                    WHITE
-                } else {
-                    DARK_GRAY
-                };
+            if should_render(pt, player_fov, map) {
                 draw_batch.set(
                     determine_pos(pt, camera),
-                    ColorPair::new(tint, BLACK),
+                    determine_color(&pt, player_fov),
                     determine_glyph(pt, map),
                 );
             }
@@ -43,4 +35,18 @@ fn determine_glyph(Point { x, y, .. }: Point, map: &Map) -> u16 {
 
 fn determine_pos(absolute_pos: Point, camera: &Camera) -> Point {
     absolute_pos - Point::new(camera.left_x, camera.top_y)
+}
+
+fn determine_color(pt: &Point, fov: &FieldOfView) -> ColorPair {
+    if fov.visible_tiles.contains(pt) {
+        ColorPair::new(WHITE, BLACK)
+    } else {
+        ColorPair::new(DARK_GRAY, BLACK)
+    }
+}
+
+fn should_render(pt: Point, fov: &FieldOfView, map: &Map) -> bool {
+    let idx = map_idx(pt.x, pt.y);
+
+    map.in_bounds(pt) && (fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx])
 }
