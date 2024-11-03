70eb524c0db5a5c552879602e0398e5435ad305f
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-11-03 10:31:26 -0500

    refactor(hud)

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -25,29 +25,21 @@ pub fn hud(ecs: &SubWorld) {
         format!("Health: {}/{}", player_health.current, player_health.max),
         ColorPair::new(WHITE, RED),
     );
+    draw_batch.submit(10000).expect("Batch error");
 
     let (player, map_level) = <(Entity, &Player)>::query()
         .iter(ecs)
         .find_map(|(entity, player)| Some((*entity, player.map_level)))
         .unwrap();
-    draw_batch.print_color_right(
-        Point::new(SCREEN_WIDTH * 2, 1),
-        format!("Dungeon Level:{}", map_level + 1),
-        ColorPair::new(YELLOW, BLACK),
-    );
-    draw_batch.submit(10000).expect("Batch error");
 
-    display_inventory(ecs);
+    display_map_level(map_level);
+    display_inventory(player, ecs);
 }
 
-fn display_inventory(ecs: &SubWorld) {
+fn display_inventory(player: Entity, ecs: &SubWorld) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(ConsoleLayer::Hud.into());
 
-    let player = <(Entity, &Player)>::query()
-        .iter(ecs)
-        .find_map(|(entity, _player)| Some(*entity))
-        .unwrap();
     let mut item_query = <(&Item, &Name, &Carried)>::query();
     let mut y = 3;
     item_query
@@ -64,6 +56,19 @@ fn display_inventory(ecs: &SubWorld) {
             ColorPair::new(YELLOW, BLACK),
         );
     }
+
+    draw_batch.submit(10000).expect("Batch error");
+}
+
+fn display_map_level(map_level: u32) {
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(ConsoleLayer::Hud.into());
+
+    draw_batch.print_color_right(
+        Point::new(SCREEN_WIDTH * 2, 1),
+        format!("Dungeon Level:{}", map_level + 1),
+        ColorPair::new(YELLOW, BLACK),
+    );
 
     draw_batch.submit(10000).expect("Batch error");
 }
