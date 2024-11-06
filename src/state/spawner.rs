2c6eb3cc39a932c482760c635fbde9ab5e34a815
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-11-06 08:06:54 -0500

    ch15: data driven enemies and items

--- src/state/spawner.rs
+++ src/state/spawner.rs
@@ -1,3 +1,7 @@
+mod template;
+
+use template::Templates;
+
 use crate::prelude::*;
 
 pub struct Spawner<'a> {
@@ -21,23 +25,17 @@ impl<'a> Spawner<'a> {
             map_builder.map.tiles[exit_idx] = TileType::Exit;
         }
 
-        map_builder
-            .monster_spawns
-            .iter()
-            .for_each(|pos| spawner.spawn_entity(*pos));
+        let template = Templates::load();
+        template.spawn_entities(
+            spawner.ecs,
+            spawner.rng,
+            level as usize,
+            &map_builder.monster_spawns,
+        );
 
         if level == 0 {
             spawner.spawn_player(map_builder.player_start);
         }
-    }
-
-    fn spawn_entity(&mut self, pos: Point) {
-        let roll = self.rng.roll_dice(1, 6);
-        match roll {
-            1 => self.spawn_healing_potion(pos),
-            2 => self.spawn_magic_mapper(pos),
-            _ => self.spawn_monster(pos),
-        };
     }
 
     fn spawn_amulet_of_yala(&mut self, pos: Point) {
@@ -50,51 +48,6 @@ impl<'a> Spawner<'a> {
                 to_cp437('/'),
                 RenderOrder::Item,
             ),
-        ));
-    }
-
-    fn spawn_healing_potion(&mut self, pos: Point) {
-        self.ecs.push((
-            Item,
-            pos,
-            Render::new(
-                ColorPair::new(WHITE, BLACK),
-                to_cp437('!'),
-                RenderOrder::Item,
-            ),
-            Name("Healing Potion".to_string()),
-            ProvidesHealing::new(6),
-        ));
-    }
-
-    fn spawn_magic_mapper(&mut self, pos: Point) {
-        self.ecs.push((
-            Item,
-            pos,
-            Render::new(
-                ColorPair::new(WHITE, BLACK),
-                to_cp437('{'),
-                RenderOrder::Item,
-            ),
-            Name("Dungeon Map".to_string()),
-            ProvidesDungeonMap,
-        ));
-    }
-
-    fn spawn_monster(&mut self, pos: Point) {
-        let (hp, name, glyph) = match self.rng.roll_dice(1, 10) {
-            1..=8 => goblin(),
-            _ => orc(),
-        };
-
-        self.ecs.push((
-            ChasingPlayer {},
-            Enemy,
-            FieldOfView::new(6),
-            Health::new(hp, hp),
-            Name(name),
-            pos,
-            Render::new(ColorPair::new(WHITE, BLACK), glyph, RenderOrder::Enemy),
         ));
     }
 
@@ -111,12 +64,4 @@ impl<'a> Spawner<'a> {
             ),
         ));
     }
-}
-
-fn goblin() -> (i32, String, FontCharType) {
-    (1, "Goblin".to_string(), to_cp437('g'))
-}
-
-fn orc() -> (i32, String, FontCharType) {
-    (2, "Orc".to_string(), to_cp437('o'))
 }
