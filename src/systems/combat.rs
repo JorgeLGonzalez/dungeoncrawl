3e34dd3d07f28d463c7a2b50a534387264d47377
Jorge Gonzalez <Jorge.Gonzalez@lexialearning.com>
2024-11-08 15:40:08 -0500

    refactor(combat): factor Damager out

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -1,3 +1,4 @@
+use super::helpers::Damager;
 use crate::prelude::*;
 
 #[system]
@@ -10,47 +11,15 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     gather_victims(ecs)
         .iter()
         .for_each(|(message, attacker, victim)| {
-            let is_player = is_player(ecs, *victim);
+            let mut damager = Damager::new(*attacker, *victim, ecs);
+            damager.attack(ecs);
 
-            damage(ecs, *attacker, *victim)
-                .filter(|health| *health < 1 && !is_player)
-                .inspect(|_| {
-                    println!("\tVictim Died :-(");
-                    commands.remove(*victim);
-                });
+            if damager.should_terminate() {
+                damager.terminate(commands);
+            }
 
             commands.remove(*message);
         });
-}
-
-fn damage(ecs: &mut SubWorld, attacker: Entity, victim: Entity) -> Option<i32> {
-    let base_damage = if let Ok(v) = ecs.entry_ref(attacker) {
-        if let Ok(damage) = v.get_component::<Damage>() {
-            damage.0
-        } else {
-            0
-        }
-    } else {
-        0
-    };
-
-    let weapon_damage: i32 = <(&Carried, &Damage)>::query()
-        .iter(ecs)
-        .filter(|(carried, _)| carried.0 == attacker)
-        .map(|(_, damage)| damage.0)
-        .sum();
-
-    let final_damage = base_damage + weapon_damage;
-
-    if let Ok(health) = ecs.entry_mut(victim).unwrap().get_component_mut::<Health>() {
-        println!("Health before attack: {}", health.current);
-        health.current -= final_damage;
-        println!("Health after attack: {}", health.current);
-
-        Some(health.current)
-    } else {
-        None
-    }
 }
 
 fn gather_victims(ecs: &SubWorld) -> Vec<(Entity, Entity, Entity)> {
@@ -58,11 +27,4 @@ fn gather_victims(ecs: &SubWorld) -> Vec<(Entity, Entity, Entity)> {
         .iter(ecs)
         .map(|(entity, attack)| (*entity, attack.attacker, attack.victim))
         .collect()
-}
-
-fn is_player(ecs: &SubWorld, victim: Entity) -> bool {
-    ecs.entry_ref(victim)
-        .unwrap()
-        .get_component::<Player>()
-        .is_ok()
 }
---
+++ src/systems/helpers/damage.rs
@@ -0,0 +1,77 @@
+use crate::prelude::*;
+
+pub struct Damager {
+    attacker: Entity,
+    killed: bool,
+    player_victim: bool,
+    victim: Entity,
+}
+
+impl Damager {
+    pub fn new(attacker: Entity, victim: Entity, ecs: &SubWorld) -> Self {
+        Self {
+            attacker,
+            killed: false,
+            player_victim: is_player(victim, ecs),
+            victim,
+        }
+    }
+
+    pub fn attack(&mut self, ecs: &mut SubWorld) {
+        let damage = self.base_damage(ecs) + self.weapon_damage(ecs);
+
+        self.killed = if let Ok(health) = ecs
+            .entry_mut(self.victim)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
+            println!("Health before attack: {}", health.current);
+            health.current -= damage;
+            println!("Health after attack: {}", health.current);
+
+            health.current < 1
+        } else {
+            false
+        }
+    }
+
+    pub fn should_terminate(&self) -> bool {
+        self.killed && !self.player_victim
+    }
+
+    pub fn terminate(&self, commands: &mut CommandBuffer) {
+        assert!(self.killed);
+        assert!(!self.player_victim);
+
+        println!("\tEnemy terminated!");
+
+        commands.remove(self.victim);
+    }
+
+    fn base_damage(&self, ecs: &SubWorld) -> i32 {
+        if let Ok(attack) = ecs.entry_ref(self.attacker) {
+            if let Ok(damage) = attack.get_component::<Damage>() {
+                damage.0
+            } else {
+                0
+            }
+        } else {
+            0
+        }
+    }
+
+    fn weapon_damage(&self, ecs: &SubWorld) -> i32 {
+        <(&Carried, &Damage)>::query()
+            .iter(ecs)
+            .filter(|(carried, _)| carried.0 == self.attacker)
+            .map(|(_, damage)| damage.0)
+            .sum()
+    }
+}
+
+fn is_player(victim: Entity, ecs: &SubWorld) -> bool {
+    ecs.entry_ref(victim)
+        .unwrap()
+        .get_component::<Player>()
+        .is_ok()
+}
